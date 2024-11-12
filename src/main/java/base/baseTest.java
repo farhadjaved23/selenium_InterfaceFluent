9ae1ce0d550c354dc39b4603211701bb34f8832f
farhadjaved23 <47040241+farhadjaved23@users.noreply.github.com>
2024-11-12 15:57:48 +0000

    integrating Selenium Grid

--- src/main/java/base/baseTest.java
+++ src/main/java/base/baseTest.java
@@ -1,26 +1,57 @@
 package base;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class baseTest {
     public WebDriver driver;
+    DesiredCapabilities capabilities = new DesiredCapabilities();
 
     @BeforeMethod
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\farha\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        driver = new ChromeDriver();
+    @Parameters("Browser")
+    public void setUp(String Browser) throws MalformedURLException {
+
+        if(Browser.equals("chrome")) {
+            capabilities.setPlatform(Platform.ANY);
+            capabilities.setBrowserName("chrome");
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.merge(capabilities);
+        }
+        else if(Browser.equals("firefox")) {
+            capabilities.setPlatform(Platform.ANY);
+            capabilities.setBrowserName("firefox");
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless=new");
+            options.merge(capabilities);
+        }
+        else if(Browser.equals("MicrosoftEdge")) {
+            capabilities.setPlatform(Platform.ANY);
+            capabilities.setBrowserName("MicrosoftEdge");
+
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("--headless=new");
+            options.merge(capabilities);
+        }
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\farha\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        //driver = new ChromeDriver();
+        //driver.manage().window().maximize();
+        driver = new RemoteWebDriver(new URL(" http://192.168.0.69:4444"),capabilities);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Use seconds, not milliseconds
-        driver.manage().window().maximize();
     }
 
     @AfterMethod
